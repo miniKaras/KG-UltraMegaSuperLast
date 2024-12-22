da7b1da00ec3abf247092050f6b6333f17aa9613
Ruslan Pikalov <rus10daa.@gmail.com>
2024-12-22 21:59:56 +0300

    test1

---
+++ src/main/java/com/cgvsu/math/Class.java
@@ -0,0 +1,28 @@
+package com.cgvsu.math;
+
+public class Class {
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    int b;
+
+    public Class(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
