a4c301106ee3b821a9f6ad9616ca780aa829c620
maksi <cmaksim666@gmail.com>
2024-12-23 02:38:03 +0300

    last commit :)

--- src/main/java/com/cgvsu/rasterization/TriangleRasterization.java
+++ src/main/java/com/cgvsu/rasterization/TriangleRasterization.java
@@ -29,6 +29,24 @@ public class TriangleRasterization {
 
         rasterizeTriangle(graphicsContext, pixelWriter, coordX, coordY, color, zBuff, deepZ, normals, textures, light, mesh);
     }
+    public static double determinator(int[][] arr) {
+        return arr[0][0] * arr[1][1] * arr[2][2] + arr[1][0] * arr[0][2] * arr[2][1] +
+                arr[0][1] * arr[1][2] * arr[2][0] - arr[0][2] * arr[1][1] * arr[2][0] -
+                arr[0][0] * arr[1][2] * arr[2][1] - arr[0][1] * arr[1][0] * arr[2][2];
+    }
+    public static double[] barizentricCoordinates(int x, int y, int[] arrX, int[] arrY) {
+        final double generalDeterminant = determinator(new int[][]{arrX, arrY, new int[]{1, 1, 1}});
+        final double alfa = Math.abs(determinator(
+                new int[][]{new int[]{x, arrX[1], arrX[2]}, new int[]{y, arrY[1], arrY[2]}, new int[]{1, 1, 1}}) /
+                generalDeterminant);
+        final double betta = Math.abs(determinator(
+                new int[][]{new int[]{arrX[0], x, arrX[2]}, new int[]{arrY[0], y, arrY[2]}, new int[]{1, 1, 1}}) /
+                generalDeterminant);
+        final double gamma = Math.abs(determinator(
+                new int[][]{new int[]{arrX[0], arrX[1], x}, new int[]{arrY[0], arrY[1], y}, new int[]{1, 1, 1}}) /
+                generalDeterminant);
+        return new double[]{alfa, betta, gamma};
+    }
 
     private static void rasterizeTriangle(
             GraphicsContext graphicsContext,
